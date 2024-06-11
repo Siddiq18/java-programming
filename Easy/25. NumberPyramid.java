be17dc3bbdf955d71ef4aed2ee69a11d00364576
Siddiq18 <113424189+Siddiq18@users.noreply.github.com>
2024-06-11 10:23:33 +0530

    Update 25. NumberPyramid.java

--- Easy/25. NumberPyramid.java
+++ Easy/25. NumberPyramid.java
@@ -4,18 +4,15 @@ public class NumberPyramid {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // Get the number of rows from the user for the first half
         System.out.print("Enter the number of rows: ");
         int rows = input.nextInt();
 
-        // Print the pattern
         printPattern(rows);
 
         input.close();
     }
 
     public static void printPattern(int rows) {
-        // Print the increasing part of the pattern
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(i + " ");
@@ -23,7 +20,6 @@ public class NumberPyramid {
             System.out.println();
         }
 
-        // Print the decreasing part of the pattern
         for (int i = rows - 1; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(i + " ");
